f3ada3fa7d972a453b10e75facc5e294c45950e7
ferchocarrillo <ferchocarrilloleon@hotmail.com>
2021-02-19 10:34:37 -0500

    1.0

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema; 
 use Carbon\Carbon;
 
 
@@ -25,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-
+        Schema::defaultStringLength(191);
     }
 }
