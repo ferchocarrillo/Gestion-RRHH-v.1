ad85c1b51c409ab302652660febd57539aad1a01
ferchocarrillo <74315654+ferchocarrillo@users.noreply.github.com>
2021-02-23 09:10:55 -0500

    23/02

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -7,33 +7,278 @@
 <script src="{{ asset('js/app.js') }}" defer></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
 integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<link rel="stylesheet" type="text/css" href="inicio/styles.css">
+<link rel="stylesheet" type="text/css" href="inicio/styles.css"><!--estilos css directos, recordar poner el nombre de la ruta-->
 <link href="{{ asset('theme/vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
 <script src="{{asset('theme/vendor/bootstrap-4.1/popper.min.js')}}"></script>
 <script src="{{asset('theme/vendor/bootstrap-4.1/bootstrap.min.js')}}"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
 integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,200;0,300;0,500;1,200&display=swap" rel="stylesheet">
+<link rel = "preconnect" href = "https://fonts.gstatic.com">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Raleway:wght@500&display=swap" rel="stylesheet">
 <body>
-<title>Mentius - Recursos Humanos</title>
+<title>Mentius-RRHH</title>
 </head>
 <body>
-<header class="hero">
+
+<header>
+<div class="capsula">
+    <div class="letras">
+        <h1>Bienvenidos </h1>
+        <h2>Mentius Recursos Humanos</h2>
+        <a href="#">Login</a>
+    </div>
+    <img src="../theme/images/recursos9.png" alt="Imagen de primer contenedor">
+</div>
+
+<!--    <header class="hero">
         <div class="textos-hero">
-            <h1>Bienvenido a Mentius Recursos Humanos</h1>
+            <h1>Bienvenido</h1>
+            <h2>Mentius Recursos Humanos</h2>
             <p>Hacemos de la excelencia nuestra mejor hoja de presentacion</p>
-    <!--<a href="#contacto">contactame</a>--> <!--boton para hacer escrol-->
+            <a href="#contacto">Login</a>
+             </header>><br><br><br><br>
+        </div>-->
+    <div class="svg-hero">
+    <svg viewBox="0 0 1320 390" >
+    <path fill-opacity="1"   d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #fff "/>
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #fff "/>
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #fff "/>
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #fff "/>
+    </svg>
+    </div>
+</header><br><br><br><br>
+
+
+    <section class="wave-contenedor webside">
+        <img src="\theme\images\recursos14.jpg" alt="">
+        <div class="contenedor-textos-main"><br><br>
+        <h2 class="titulo left">¿Que Son Los Recursos Humanos?</h2>
+        <p class="parrafo">Los recursos humanos son un departamento dentro de las empresas en el que se gestiona todo lo
+             relacionado con las personas que trabajan en ella. Esto incluiría desde el reclutamiento, selección,
+             contratación, onboarding o bienvenida, formación, promoción, nóminas, contratos y despidos. En resumidas
+             cuentas, el departamento de recursos humanos debe trabajar para todas las personas que forman parte del
+             equipo humano de la empresa. Los recursos humanos son indispensables para cualquier empresa que necesite
+             crecer y contratar a los mejores trabajadores para cada puesto o ayudar a las personas que trabajan en la
+             empresa a seguir desarrollándose, formándose y creciendo.</p>
+        <!--<a href="" class="cta">Conoce más</a>  boton -->
         </div>
-      <div class="svg-hero">
-          <svg viewBox="0 0 1320 280" >
-             <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1110 500 L0 500" fill=" #fff "/>
-             <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 500 L0 500" fill=" #fff "/>
-             <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 500 L0 500" fill=" #fff "/>
-             <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 500 L0 500" fill=" #fff "/>
-          </svg>
-      </div>
-</header>
+        </section>
+    <section class="info">
+    <div class="contenedor">
+    <h1>No contratamos personas que hace el trabajo por dinero, sino los que lo hace por amor.</h1><br><br><br><br>
+    <p>Una máquina puede hacer el trabajo de cincuenta hombres normales. Ninguna máquina puede hacer el trabajo de un  hombre extraordinario.</p>
+    </div>
+    </section><br><br><br>
+
+    <div class="contenedor last-section">
+        <div class="contenedor-textos-main">
+        <h2 class="titulo left">7 Funciones De Recursos Humanos</h2><br><br>
+        <p class="parrafo">En calidad Mentius, tenemos el proposito de ayudarte a crear grandes experiencias de la forma correcta,
+        entendiendo mejor tu función y su importancia. Queremos estar siempre presente cuando alcances tus metas, cuando cumplas con
+        tus objetivos. Nosotros creemos en ti por eso buscaremos siempre la mejor manera de crecer juntos.</p>
+        <!--<a href="" class="cta">learn more</a>-->
+        </div>
+        <div class="ratio ratio-4x3">
+       <img src="\theme\images\recursos6.png" alt="">
+        </div>
+        </div>
+
+    <section class="cards contenedor">
+    <h2 class="titulo">Lo que debes saber de recursos humanos</h2>
+    <div class="content-cards">
+    <article class="card">
+    <i class="fas fa-clipboard-list"></i>
+    <div class="contenedor-textos-main">
+    <h3>Alinear la compañía con Recursos Humanos</h3>
+    <p>El área de Recursos Humanos tiene que tener una alta capacidad de persuasión a los altos líderes de una empresa al momento de empezar con la
+    planificación estratégica o asignación de nuevos recursos.</p>
+       <!-- <a href="#" class="cta">Conoce más </a>-->
+    </div>
+    </article>
+    <article class="card">
+    <i class="fas fa-handshake"></i>
+    <div class="contenedor-textos-main">
+    <h3>Ayudar a atraer y retener al mejor talento</h3>
+    <p>Los CEOs buscan que el área de Recursos Humanos indague y encuentren el mejor talento para que haga parte de la compañía e impulse la estrategia
+    para alcanzar las metas.</p>
+     <!-- <a href="" class="cta">Conoce más </a>-->
+    </div>
+    </article>
+    <article class="card">
+        <i class="fas fa-chalkboard-teacher"></i>
+        <div class="contenedor-textos-main">
+        <h3>Ejecutar un robusto proceso de onboarding</h3>
+        <p> Un proceso de alta calidad permite instruir y enseñar sobre la historia de la compañía, y conocer la generalidad de la industria a la cual
+        pertenece la compañía.</p>
+         <!-- <a href="" class="cta">Conoce más </a>-->
+        </div>
+        </article>
+    <article class="card">
+        <i class="far fa-eye"></i>
+    <div class="contenedor-textos-main">
+    <h3>Enfocarse en el compromiso del colaborador</h3>
+    <p>La gran mayoría de las empresas de hoy no miden ni gestionan el compromiso de los colaboradores. Es aquí donde Recursos Humanos juega un papel muy
+    importante a la hora de impulsar el compromiso y la motivación de los colaboradores.</p>
+     <!-- <a href="" class="cta">Conoce más</a>-->
+    </div>
+    </article>
+    </div>
+    </section><br>
+
+    <section class="info-last">
+    <section class="galeria contenedor">
+    <div class="contenedor">
+    <h2 class="titulo">GRUPO DE TRABAJO</h2><br>
+    <article class="galeria-cont">
+        <img src="\theme\images\grupo1.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo2.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo3.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo4.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo5.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo7.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo8.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo9.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo9.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo10.jpg" alt="imagenes de grupo de trabajo ">
+        <img src="\theme\images\grupo6.jpeg" alt="imagenes de grupo de trabajo ">
+    </article>
+    </div>
+    </section>
+    <div class="svg-wave">
+    <svg viewBox="0 0 1320 290" >
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1110 300 L0 300" fill=" #000000fa"/>
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #000000fa "/>
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #000000fa "/>
+    <path fill-opacity="0.5" d="M0,192 C220,100,440,100,660,192 C880,290,1100,290,1320,192 L1320 300 L0 300" fill=" #000000fa "/>
+    </svg>
+    </div>
+    </section>
+
+    <div class="container-footer">
+    <footer>
+    <div class="logo-footer">
+    <img src="\theme\images\isotipo-slogan.png" alt="">
+    </div>
+    <div class="redes-footer">
+    <a href="https://www.facebook.com/mentiussas/"><i class="fab fa-facebook-f icon-redes-footer"></i></a>
+    <a href="#"><i class="fab fa-twitter icon-redes-footer"></i></a>
+    <a href="#"><i class="fab fa-instagram icon-redes-footer"></i></a>
+    <a href="https://www.linkedin.com/company/mentius-s-a-s/?originalSubdomain=co"><i class="fab fa-linkedin-in icon-redes-footer"></i></i></a>
+    </div>
+    <hr>
+    <h6><i class="fa fa-copyright" aria-hidden="true">2021 Mentius Colombia, Todos los Derechos Reservados Diseñado y Desarrollado por Mauricio Giraldo</i> </h6>
+    </footer>
+    </div>
+
+
+<script src="https://kit.fontawesome.com/026ce6da71.js" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
