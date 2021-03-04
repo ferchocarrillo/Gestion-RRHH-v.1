9c2e303be3c6103a7acfe3bdac10719606835028
ferchocarrillo <ferchocarrilloleon@hotmail.com>
2021-03-04 15:41:48 -0500

    Update ContratacionController.php

--- app/Http/Controllers/ContratacionController.php
+++ app/Http/Controllers/ContratacionController.php
@@ -163,7 +163,7 @@ class ContratacionController extends Controller
         $date = Carbon::now();
         $aprobaciones  = Aprobacion::all();
         $datoscontratacion =request()->except(['_token','_method']);
-        Contratacion::where('id','=',$id)->update($datoscontratacion);
+        Contratacion::where('id_filtro','=',$id)->update($datoscontratacion);
         $contratacion=Contratacion::findOrFail($id);
         //return response()->json($contratacion);
         return view('contratacion.edit', compact('contratacion','entFinalizacion','date','aprobaciones'));
